54d5c8d215836d1a72b411efcc4ce2441579e725
Eddie <eocampo52@gmail.com>
2025-08-19 17:03:50 -0500

    ai recommendations

--- server/index.js
+++ server/index.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 const cors = require('cors');
-const connect_DB = require('./connect_DB/connect_DB');
-
-const path = require('path');
-
-// Debug logging
-console.log('Current directory:', __dirname);
-console.log('Resolved connect_DB path:', path.resolve('./connect_DB/connect_DB'));
-console.log('Directory contents:', require('fs').readdirSync('./'));
-
-
-
+const connect_DB = require('./connect_DB/connect_DB.js');
 
 const app = express();
+
+dotenv.config();
+
+console.log('DB_URL from env:', process.env.DB_URL);
+console.log('Connect DB module path:', require.resolve('./connect_DB/connect_DB.js'));
 
 app.use(express.json());
 

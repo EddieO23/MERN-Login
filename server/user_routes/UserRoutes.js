4df77aaa8859ec39f20c3f6919790cd4bb8326ba
Eddie <eocampo52@gmail.com>
2025-08-19 17:19:49 -0500

    ai recommendations

---
+++ server/user_routes/UserRoutes.js
@@ -0,0 +1,24 @@
+const {
+  register,
+  login,
+  verifyUser,
+  resendVerification,
+  updateUser,
+  forgotPassword,
+  verifyPasswordOTP,
+  getUserDetails,
+} = require('../user_controller/UserController');
+const JWT_AUTH = require('../middleware/JWT_Auth');
+
+const router = require('express').Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.put('/updateUser', JWT_AUTH, updateUser);
+router.get('/verify/:token', verifyUser);
+router.get('/resendVerification/:token', resendVerification);
+router.post('/resetPassword', forgotPassword);
+router.post('/verifyPasswordOTP', verifyPasswordOTP);
+router.get('/', JWT_AUTH, getUserDetails);
+
+module.exports = router;
